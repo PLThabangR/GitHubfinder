9af8460363d5758fe7a602a9a4b87a8446549c60
PLThabangR <rakgoropothabang@gmail.com>
2020-05-15 00:46:28 +0200

    Used asyn to fetch git data and await to get the res

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { MDBBtn, MDBCol, MDBContainer, MDBRow } from "mdbreact";
 import "./index.css";
 import Navbar from "./Components/Layout/Navbar";
 import Users from "./Components/User/Users";
-
+import axios from 'axios'
 class App extends Component {
  
-componentDidMount(){
-  
+async componentDidMount(){
+ const res= await axios.get('https://api.github.com/users');
+
+ console.log(res.data);
 }
 
   render() {
